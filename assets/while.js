79451c5480d066435ebd1dac303cd822b4ac4198
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-02 18:11:46 +0300

    add a do while loop

--- assets/while.js
+++ assets/while.js
@@ -1,9 +1,25 @@
+// const cats = ["Lion","Zebra","Bufallo"]
+
+// let myFavoriteCats = "My favorite wild animals are ";
+
+// let i = 0;
+// while ( i < cats.length){
+//     if (i === cats.length - 1){
+//         myFavoriteCats += ` and ${cats[i]}.`;
+//     }
+//     else{
+//         myFavoriteCats += `${cats[i]},`;
+//     }
+
+//     i++
+// }
+ 
 const cats = ["Lion","Zebra","Bufallo"]
 
 let myFavoriteCats = "My favorite wild animals are ";
 
 let i = 0;
-while ( i < cats.length){
+  do{
     if (i === cats.length - 1){
         myFavoriteCats += ` and ${cats[i]}.`;
     }
@@ -12,5 +28,4 @@ while ( i < cats.length){
     }
 
     i++
-}
-console.log(myFavoriteCats);
+}while ( i < cats.length)
